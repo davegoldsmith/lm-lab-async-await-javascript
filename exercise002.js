7fc276f6fbc3560de24c83c65834bea11b215dab
Dave Goldsmith <davegoldsmith@hotmail.com>
2023-01-26 16:31:34 +0000

    Completed exercise 2, task 1 : updated fetchData to be async/awaity

--- exercise002.js
+++ exercise002.js
@@ -1,6 +1,8 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const cats = "https://api.thecatapi.com/v1/images/search?limit=10";
+const bbc = "https://bbc.co.uk"
 
 const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
@@ -10,3 +12,16 @@ const fetchData = (apiEndPoint) => {
 };
 
 fetchData(jsonTypicode);
+
+// Task 1 : implement fetchData with async/await
+const asyncFetchData = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    const json = await response.json();
+    console.log(json);
+  } catch(error) {
+    console.log(`Error occurred: ${error}`);
+  }
+};
+
+asyncFetchData(bbc);
